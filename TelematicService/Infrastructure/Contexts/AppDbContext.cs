9bbaec5ef87224a7f6dbce322b646c9eeb282279
Michael <gadreelazazel@gmail.com>
2026-02-25 18:15:48 +0100

    Setup template database context

---
+++ TelematicService/Infrastructure/Contexts/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace TelematicService.Infrastructure.Contexts
+{
+    // Application DbContext for EF Core
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+    }
+}
