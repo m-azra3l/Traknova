3043ab8e66f6ce73fadc81f43fc9a497420f560f
Michael <gadreelazazel@gmail.com>
2026-02-25 19:06:50 +0100

    Added utility class for handling json deserialization of telemetry payloads for test purpose

---
+++ TelematicService/Application/MapServices/PayloadDeserializer.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using TelematicService.Application.Dtos;
+
+namespace TelematicService.Application.MapServices
+{
+    public static class PayloadDeserializer
+    {
+        // Utility class for handling JSON deserialization of telemetry payloads
+        // Provides a static method to convert raw JSON into strongly typed DTOs
+        public static class TelematicsDeserializer
+        {
+            // Deserialize a JSON string into a list of TelematicsDto objects
+            public static List<TelematicsDto> Deserialize(string jsonPayload)
+            {
+                // Configure JSON serializer options.
+                // PropertyNameCaseInsensitive ensures that JSON property names
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                return JsonSerializer.Deserialize<List<TelematicsDto>>(jsonPayload, options) ?? [];
+            }
+        }
+    }
+}
