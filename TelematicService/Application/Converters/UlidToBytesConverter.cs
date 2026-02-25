f0e23033a264df1f5f62ae35415deea7014b70ee
Michael <gadreelazazel@gmail.com>
2026-02-25 18:39:41 +0100

    Add Ulidconverter, entities to db context and configure model builder to use converter

---
+++ TelematicService/Application/Converters/UlidToBytesConverter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace TelematicService.Application.Converters
+{
+    public class UlidToBytesConverter : ValueConverter<Ulid, byte[]>
+    {
+        public UlidToBytesConverter()
+            : base(
+                ulid => ulid.ToByteArray(),   // ULID → byte[]
+                bytes => new Ulid(bytes)      // byte[] → ULID
+            )
+        { }
+    }
+}
--- TelematicService/Infrastructure/Contexts/AppDbContext.cs
+++ TelematicService/Infrastructure/Contexts/AppDbContext.cs
@@ -1,10 +1,43 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
+using TelematicService.Application.Converters;
+using TelematicService.Domain.Entities;
 
 namespace TelematicService.Infrastructure.Contexts
 {
     // Application DbContext for EF Core
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
+        // Configure model builder to use ulid converter
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var ulidConverter = new UlidToBytesConverter();
+
+            modelBuilder.Entity<CrashDetection>(entity =>
+            {
+                entity.Property(e => e.Id)
+                      .HasConversion(ulidConverter)
+                      .HasColumnType("binary(16)")
+                      .IsRequired();
+
+                entity.Property(e => e.TelematicsId)
+                      .HasConversion(ulidConverter)
+                      .HasColumnType("binary(16)")
+                      .IsRequired();
+            });
+
+            modelBuilder.Entity<Telematics>(entity =>
+            {
+                entity.Property(e => e.Id)
+                      .HasConversion(ulidConverter)
+                      .HasColumnType("binary(16)")
+                      .IsRequired();
+            });
+        }
+
+        // DbSet representing the CrashDetection table in the database
+        public DbSet<CrashDetection> CrashDetections { get; set; }
+
+        // DbSet representing the Telematics table in the database
+        public DbSet<Telematics> Telematics { get; set; }
     }
 }
